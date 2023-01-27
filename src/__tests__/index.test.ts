a704d97fd1cc858ec67c06fb349f68ef5012169f
Cyril Okidi <okidicyril@gmail.com>
2023-01-27 11:27:03 +0300

    Refractor test case

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,15 +1,38 @@
-import HEX2ASCII, { EFieldName } from "../index";
+import HEX2ASCII, { EFieldName, IOptions } from "../index";
 
-describe("HEX2ASCII", () => {
-    test('Transmission Date', () => {
-        const data = "323032322d31312d31352c31323a31363a35352c3836383830353036313235393734362c4133452d3942484741332c4b4241313736542c302c3439382c312c31312c33362e3835353630362c45282b292c312e3235393237312c53282d292c302c302c3023"
-        const hexToAscii = new HEX2ASCII(data, {
+export interface ITestResult {
+    date: string;
+}
+export interface ITestData {
+    input: string;
+    options: IOptions;
+    result: ITestResult;
+}
+
+const data: ITestData[] = [
+    {
+        input: "323032322d31312d31352c31323a31363a35352c3836383830353036313235393734362c4133452d3942484741332c4b4241313736542c302c3439382c312c31312c33362e3835353630362c45282b292c312e3235393237312c53282d292c302c302c3023",
+        options: {
             fields: [
                 {
                     name: EFieldName.DATE,
                 }
             ]
+        },
+        result: {
+            date: "2022-11-15"
+        }
+    }
+];
+
+describe("HEX2ASCII", () => {
+    data.map((d: ITestData, i: number) => {
+        describe(`Sample data ${i + 1}`, () => {
+            const hexToAscii = new HEX2ASCII(d.input, d.options);
+
+            test('Transmission Date', () => {
+                expect(hexToAscii.date).toEqual(d.result.date);
+            })
         });
-        expect(hexToAscii.date).toEqual('2022-11-15')
     });
 });
